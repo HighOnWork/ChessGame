070b9ecf65be9e2a75911a952aa3166f3abdd231
Muhammad Ahmad <highonworking@gmail.com>
2025-11-27 17:50:15 +0500

    trying my best man

--- chess_board.py
+++ chess_board.py
@@ -3,11 +3,14 @@ import tkinter as tk
 class chess_board:
     def __init__(self):
         self.window_maker = tk.Tk()
-        self.canvas = tk.Canvas()
+        self.canvas = tk.Canvas(height=1000, width=1000)
     def create_board(self):
         self.window_maker.minsize(600, 600)
-        self.canvas.create_line(0, 0, 300, 300)
         self.window_maker.config(bg="brown")
+    def lining(self):
+        for i in range(8):
+            self.canvas.create_line(0, 0, 0, 600)
+
     def config(self):
         self.canvas.grid(column=0, row=0)
         self.window_maker.mainloop()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from chess_board import chess_board
 
 new_chess_board = chess_board()
+
 new_chess_board.create_board()
+new_chess_board.lining()
 new_chess_board.config()
