bc7e834cf0350934add3614b20be24b216ece2a8
Muhammad Ahmad <highonworking@gmail.com>
2025-11-27 18:13:32 +0500

    why this shi not working lol

--- chess_board.py
+++ chess_board.py
@@ -6,10 +6,16 @@ class chess_board:
         self.canvas = tk.Canvas(height=1000, width=1000)
     def create_board(self):
         self.window_maker.minsize(600, 600)
-        self.window_maker.config(bg="brown")
+        self.canvas.config(bg="brown")
     def lining(self):
+        x1 = 1
+        Y1 = 0
+        x2 = 1
+        Y2 = 1000
         for i in range(8):
-            self.canvas.create_line(0, 0, 0, 600)
+            self.canvas.create_line(x1, Y1, x2, Y2)
+            x1 = round(x1 * 66.67)
+            x2 = round(x2 * 66.67)
 
     def config(self):
         self.canvas.grid(column=0, row=0)
