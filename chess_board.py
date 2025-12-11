ded8c832d7cffe9b9b1ae969e239db87b8401da1
Muhammad Ahmad <highonworking@gmail.com>
2025-12-11 23:51:08 +0500

    so the chess board is about done just the graphics the real engine is still to be made

--- chess_board.py
+++ chess_board.py
@@ -4,18 +4,45 @@ class chess_board:
     def __init__(self):
         self.window_maker = tk.Tk()
         self.canvas = tk.Canvas(height=1000, width=1000)
+        self.SPACING = 125
+        self.BOTTOM_Y_COORDINATE = 1000
+        self.TOP_Y_COORDINATE = 0
+        self.RIGHT_MOST_X_COORDINATE = 1000
+        self.LEFT_MOST_X_COORDINATE = 0
+
     def create_board(self):
         self.window_maker.minsize(600, 600)
-        self.canvas.config(bg="brown")
+        self.canvas.config(bg="#582917")
+     
     def lining(self):
-        x1 = 1
-        Y1 = 0
-        x2 = 1
-        Y2 = 1000
-        for i in range(8):
-            self.canvas.create_line(x1, Y1, x2, Y2)
-            x1 = round(x1 * 66.67)
-            x2 = round(x2 * 66.67)
+        for i in range(8): 
+            x = self.SPACING * i
+            self.canvas.create_line(x, self.TOP_Y_COORDINATE, x, self.BOTTOM_Y_COORDINATE)
+
+        for j in range(8): 
+            y = self.SPACING * j
+            self.canvas.create_line(self.LEFT_MOST_X_COORDINATE, y, self.RIGHT_MOST_X_COORDINATE, y)
+
+    def numbers_and_alphabets(self):
+        self.window_maker.title("Chess Engine")
+        Y_COORDINATE_FOR_BOTTOM_ALPHABETS = self.BOTTOM_Y_COORDINATE - 10
+        Y_COORDINATE_FOR_TOP_ALPHABETS = self.TOP_Y_COORDINATE + 15
+        num_spacing = self.SPACING // 2
+        numbers_for_board = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
+        for num in numbers_for_board:
+            self.canvas.create_text(
+                num_spacing, Y_COORDINATE_FOR_BOTTOM_ALPHABETS,
+                text=num,
+                fill="white",
+                font=("Halvetica", 16, "bold")
+            )
+            self.canvas.create_text(
+                num_spacing, Y_COORDINATE_FOR_TOP_ALPHABETS,
+                text=num,
+                fill="white",
+                font=("Halvetica", 16, "bold")
+            )
+            num_spacing += self.SPACING
 
     def config(self):
         self.canvas.grid(column=0, row=0)
--- main.py
+++ main.py
@@ -4,4 +4,5 @@ new_chess_board = chess_board()
 
 new_chess_board.create_board()
 new_chess_board.lining()
+new_chess_board.numbers_and_alphabets()
 new_chess_board.config()
