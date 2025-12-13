1a1a01c0ed38b296c7f3d74bd38f717f7e43df24
Muhammad Ahmad <highonworking@gmail.com>
2025-12-13 05:41:32 +0500

    Black Pawns done

--- chess_pieces.py
+++ chess_pieces.py
@@ -10,12 +10,14 @@ class ChessPieces:
         self.new_width = 75
         self.new_height = 75
         self.center_x = 63
-        self.center_y = 74
+        self.center_y = 190
     def black_pawn(self):
         global img_ref
         unconvertedImage = Image.open(r"C:\Users\HighO\OneDrive\Documents\GitHub\ChessEngine\BlackPawn.png")
         resized_image = unconvertedImage.resize((self.new_width, self.new_height), Image.Resampling.LANCZOS)
         img_ref = ImageTk.PhotoImage(resized_image) 
-        for _ in range(7):
-            self.canvas.create_image(self.center_x, self.center_y, image=img_ref)
+        new_x_for_each_pawn = self.center_x
+        for _ in range(8):
+            self.canvas.create_image(new_x_for_each_pawn, self.center_y, image=img_ref)
+            new_x_for_each_pawn += 125
         
