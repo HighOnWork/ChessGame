c00a3db47294de72c12c091b65bfc3e022844773
Muhammad Ahmad <highonworking@gmail.com>
2025-12-13 06:46:55 +0500

    code cleaned and shortened

--- chess_pieces.py
+++ chess_pieces.py
@@ -12,23 +12,25 @@ class ChessPieces:
         self.NEW_HEIGHT = 75
         self.CENTER_X = 63
         self.CENTER_Y = 190
+    def image_taketh_and_changeth(self, fileLocation):
+        unconvertedImage = Image.open(fileLocation)
+        resized_image = unconvertedImage.resize((self.NEW_WIDTH, self.NEW_HEIGHT), Image.Resampling.LANCZOS)
+        img_converted = ImageTk.PhotoImage(resized_image) 
+        return img_converted
     def black_pawn(self):
         global img_ref_black
-        unconvertedImage = Image.open(r"C:\Users\HighO\OneDrive\Documents\GitHub\ChessEngine\BlackPawn.png")
-        resized_image = unconvertedImage.resize((self.NEW_WIDTH, self.NEW_HEIGHT), Image.Resampling.LANCZOS)
-        img_ref_black = ImageTk.PhotoImage(resized_image) 
+        img_ref_black = self.image_taketh_and_changeth(".\\BlackPawn.png")
         new_x_for_each_pawn = self.CENTER_X
         for _ in range(8):
             self.canvas.create_image(new_x_for_each_pawn, self.CENTER_Y, image=img_ref_black)
             new_x_for_each_pawn += 125
     def white_pawn(self):
         global img_ref_white
-        unconvertedImage = Image.open(r"C:\Users\HighO\OneDrive\Documents\GitHub\ChessEngine\WhitePawn.png")
-        resized_image = unconvertedImage.resize((self.NEW_WIDTH, self.NEW_HEIGHT), Image.Resampling.LANCZOS)
-        img_ref_white = ImageTk.PhotoImage(resized_image) 
+        img_ref_white = self.image_taketh_and_changeth(".\\WhitePawn.png")
         new_x_for_each_pawn = self.CENTER_X
         new_y_for_each_pawn = 1000 - self.CENTER_Y
         for _ in range(8):
             self.canvas.create_image(new_x_for_each_pawn, new_y_for_each_pawn, image=img_ref_white)
             new_x_for_each_pawn += 125
+    
         
