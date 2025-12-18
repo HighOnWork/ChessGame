3f0a32fb86ab495685da2b750e748bc6c4e04d97
Muhammad Ahmad <highonworking@gmail.com>
2025-12-18 10:32:45 +0500

    The next spaces for the pawn indeed work to an extent :)

--- movement_of_pieces.py
+++ movement_of_pieces.py
@@ -4,13 +4,24 @@ class movement_of_indivisual_pieces:
         # self.X1, self.Y1 = 0, 250
         self.SIDE_LENGTH = 125
         # self.X2, self.Y2 = self.X1 + self.SIDE_LENGTH, self.Y1 + self.SIDE_LENGTH
-    def pawn_moment(self, event, pawn_y_position, pawn_x_position):
+    
+    def button_click(self, event):
+        print("Button Clicked")
+
+    def black_pawns_movement(self, event, pawn_y_position, pawn_x_position):
+        spaces_to_move = []
+
         X1, Y1 = pawn_x_position - 125 // 2, (pawn_y_position - 125 // 2) + self.SIDE_LENGTH
         X2, Y2 = X1 + self.SIDE_LENGTH, Y1 + self.SIDE_LENGTH
+
         if pawn_y_position <= 190:
             for _ in range(2):
-                self.canvas.create_rectangle(X1, Y1, X2, Y2, fill="orange", width=2)
+                spaces_to_move.append(self.canvas.create_rectangle(X1, Y1, X2, Y2, fill="orange", width=2))
                 Y1 += 125
                 Y2 += 125
         else:
-            self.canvas.create_rectangle(X1, Y1, X2, Y2, fill="orange", width=2)
+            spaces_to_move.append(self.canvas.create_rectangle(X1, Y1, X2, Y2, fill="orange", width=2))
+
+        for spaces in spaces_to_move:
+            self.canvas.tag_bind(spaces, "<Button-1>", self.button_click)
+        
